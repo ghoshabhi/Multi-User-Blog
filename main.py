460c33b0e3e14889527a48fbf0aa5d86cb90c898
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-03 21:29:20 -0800

    Feat: Separate BlogHandler

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from utility import check_secure_val, hash_str, make_secure_val,\
                     valid_username, valid_password, valid_email
 from models import User, Post, UserPhoto, Likes, Comments
 from utility import showCount, filterKey
+from handlers import BlogHandler
 
 from datetime import datetime
 from dateutil import tz
@@ -19,42 +20,42 @@ from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext import blobstore
 from google.appengine.api import images
 
-template_dir = os.path.join(os.path.dirname(__file__), 'views')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
-                              autoescape=True)
-
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
-
-
-jinja_env.filters['filterKey'] = filterKey
-jinja_env.filters['showCount'] = showCount
+# template_dir = os.path.join(os.path.dirname(__file__), 'views')
+# jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+#                               autoescape=True)
+#
+# def render_str(template, **params):
+#     t = jinja_env.get_template(template)
+#     return t.render(params)
 
 
-class BlogHandler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
+# jinja_env.filters['filterKey'] = filterKey
+# jinja_env.filters['showCount'] = showCount
 
-    def render_str(self, template, **params):
-        return render_str(template, **params)
 
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
-
-    def get_user_from_cookie(self):
-        random = self.check_for_valid_cookie()
-        if random:
-            return User.get_by_id(int(random))
-        else:
-            return None
-    def check_for_valid_cookie(self):
-        random = self.request.cookies.get('random')
-        if random:
-            is_valid_cookie = check_secure_val(random)
-            if is_valid_cookie:
-                return self.request.cookies.get('random').split("|")[0]
-        return None
+# class BlogHandler(webapp2.RequestHandler):
+#     def write(self, *a, **kw):
+#         self.response.out.write(*a, **kw)
+#
+#     def render_str(self, template, **params):
+#         return render_str(template, **params)
+#
+#     def render(self, template, **kw):
+#         self.write(self.render_str(template, **kw))
+#
+#     def get_user_from_cookie(self):
+#         random = self.check_for_valid_cookie()
+#         if random:
+#             return User.get_by_id(int(random))
+#         else:
+#             return None
+#     def check_for_valid_cookie(self):
+#         random = self.request.cookies.get('random')
+#         if random:
+#             is_valid_cookie = check_secure_val(random)
+#             if is_valid_cookie:
+#                 return self.request.cookies.get('random').split("|")[0]
+#         return None
 
 
 class HomeHandler(BlogHandler):
